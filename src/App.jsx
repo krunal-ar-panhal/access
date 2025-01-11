c437eaa6cd0e7fea585734dfcd1bf2e90473f430
krunal-ar-panhal <krunalapnchalkp2123@gmail.com>
2025-01-11 13:01:48 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
     <>
     <div> hii</div>
       {/* <BrowserRouter>  */}
-      <Navbar/>
+      {/* <Navbar/> */}
       <Header/>
         {/* <Routes>
           <Route path='/' element={<Home/>}/>
